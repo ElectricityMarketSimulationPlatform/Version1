10de14c59476435cace70c6be98c3d1fa7ec3843
EurekaQi <75938250+EurekaQi@users.noreply.github.com>
2020-12-17 22:27:04 +0800

    主程序添加

---
+++ ElectricityMarketSimulationPlatform/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectricityMarketSimulationPlatform.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+
+        [ProtectedPersonalData]
+        [RegularExpression("[0-9]{10}")]
+        [Display(Name = "学号")]
+        public string IdNumber { get; set; }
+    }
+}
---
+++ ElectricityMarketSimulationPlatform/Startup.cs
@@ -0,0 +1,72 @@
+using ElectricityMarketSimulationPlatform.Data;
+using ElectricityMarketSimulationPlatform.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectricityMarketSimulationPlatform
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<ApplicationUserDbContext>(options =>
+            {
+                options.UseMySql(connectionString);
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 1;
+            })
+                .AddEntityFrameworkStores<ApplicationUserDbContext>();
+        }
+ 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
